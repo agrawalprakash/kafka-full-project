3cd74cb37e2670eebfe5775d239bba4cc0461fd1
agrawalprakash <33377123+agrawalprakash@users.noreply.github.com>
2024-08-03 09:34:11 +0530

    Library Event Controller Unit Test

---
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerTest.java
@@ -0,0 +1,48 @@
+package com.learnkafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.library_events_producer.controller.LibraryEventsController;
+import com.learnkafka.library_events_producer.domain.Book;
+import com.learnkafka.library_events_producer.domain.LibraryEvent;
+import com.learnkafka.library_events_producer.domain.LibraryEventType;
+import com.learnkafka.library_events_producer.producer.LibraryEventsProducer;
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LibraryEventsController.class)
+class LibraryEventsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    LibraryEventsProducer libraryEventsProducer;
+
+    @Test
+    void postLibraryEvent() throws Exception {
+
+        var json = objectMapper.writeValueAsString(
+                new LibraryEvent(11, LibraryEventType.UPDATE, new Book(11, "aa", "aa")));
+
+        when(libraryEventsProducer.sendLibraryEvent_approach3(isA(LibraryEvent.class))).thenReturn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/libraryEvent").content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+    }
+}
